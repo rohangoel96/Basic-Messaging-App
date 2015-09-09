d1420ba74f31df1a9f134c4c86f6f995d5e6ddf4
Rohan Goel <rohangoel0296@gmail.com>
2015-09-09 20:26:35 +0530

    starting to code

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Server {
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output ;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	
+
+}
