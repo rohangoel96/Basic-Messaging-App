1aa0100dd1e6fdb7ca3633ad539f25c678d5cadb
Rohan Goel <rohangoel0296@gmail.com>
2015-09-10 19:24:12 +0530

    GUI for client

--- src/Client.java
+++ src/Client.java
@@ -14,6 +14,38 @@ public class Client extends JFrame{
 	private String serverIP;
 	private Socket connection;
 	
+	/**
+	 * Server sits on a public domain and can be accessed by anyone
+	 * Here the client can connect to only the server
+	 */
+	
+	public Client(String host){ //IP of the server
+		
+		super("Rohan's Client");
+		serverIP = host;
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+				
+				new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent event) {
+						sendData(event.getActionCommand());
+						userText.setText("");
+						
+					}
+				}				
+				);
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow),BorderLayout.CENTER);
+		setSize(400,600);
+		setVisible(true);
+		
+		
+	}
+	
 	
 	
 	
